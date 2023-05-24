3362a9dacd29d713facba7f143cfdf892a4f6034
JoJoGDS <george.latiko@gmail.com>
2023-05-24 15:17:58 -0700

    1. simple shell 0.1

--- builtin.c
+++ builtin.c
@@ -3,9 +3,9 @@
 /**
  * _myexit - exits the shell
  * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: exits with a given exit status
- *         (0) if info.argv[0] != "exit"
+ * constant function prototype.
+ * Return: exits with a given exit status
+ * (0) if info.argv[0] != "exit"
  */
 int _myexit(info_t *info)
 {
@@ -32,8 +32,8 @@ int _myexit(info_t *info)
 /**
  * _mycd - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
+ * constant function prototype.
+ * Return: Always 0
  */
 int _mycd(info_t *info)
 {
@@ -48,7 +48,7 @@ int _mycd(info_t *info)
 		dir = _getenv(info, "HOME=");
 		if (!dir)
 			chdir_ret = /* TODO: what should this be? */
-			    chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
 		else
 			chdir_ret = chdir(dir);
 	}
@@ -62,7 +62,7 @@ int _mycd(info_t *info)
 		}
 		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
 		chdir_ret = /* TODO: what should this be? */
-		    chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
 	}
 	else
 		chdir_ret = chdir(info->argv[1]);
@@ -82,85 +82,16 @@ int _mycd(info_t *info)
 /**
  * _myhelp - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: 0 on success, 1 on failure
+ * constant function prototype.
+ * Return: Always 0
  */
 int _myhelp(info_t *info)
 {
-	char **arg;
-	char *builtin;
+	char **arg_array;
 
-	arg = info->argv;
-	if (arg[1] == NULL)
-	{
-		help();
-		return (0);
-	}
-	builtin = help_flag_check(info, arg);
-	if (!builtin)
-		return (0);
-	if (_strcmp(builtin, "alias") == 0)
-		help_alias(info);
-	else if (_strcmp(builtin, "cd") == 0)
-		help_cd(info);
-	else if (_strcmp(builtin, "echo") == 0)
-		help_echo(info);
-	else if (_strcmp(builtin, "exit") == 0)
-		help_exit(info);
-	else if (_strcmp(builtin, "help") == 0)
-		help_help(info);
-	else if (_strcmp(builtin, "history") == 0)
-		help_history(info);
-	else if (_strcmp(builtin, "pwd") == 0)
-		help_pwd(info);
-	else
-	{
-		printf("-bash: help: no help topics match `%s'.", builtin);
-		printf("  Try `help help' or `man -k %s' or `info %s'.\n", builtin, builtin);
-		info->status = 1;
-		return (1);
-	}
-	info->status = 0;
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array); /* temp att_unused workaround */
 	return (0);
-}
-
-/**
- * help_flag_check - checks if there is valid flag on help built-in
- * @info: struct containing commands from line
- * @arg: arguments entered when using help
- * Return: builtin to search for
- */
-char *help_flag_check(info_t *info, char **arg)
-{
-	char *builtin = arg[1];
-	int i = 1, j = 0;
-
-	if (arg[1][j] == '-')
-	{
-		for (j = 1; j < _strlen(arg[i]); j++)
-		{
-			if (arg[1][j] == 's')
-				info->help = "s";
-			if (arg[1][j] == 'd')
-			{
-				info->help = "d";
-				break;
-			}
-		}
-		for (j = 1; j < _strlen(arg[1]); j++)
-		{
-			if (arg[1][j] != 's' &&
-			    arg[1][j] != 'd' &&
-			    arg[1][j] != 'm')
-			{
-				printf("-hsh: help: %c: invalid option\n", arg[i][j]);
-				printf("help: usage: help [-dms] [pattern ...]\n");
-				info->help = NULL;
-				return (NULL);
-			}
-		}
-		return (arg[2]);
-	}
-	else
-		return (builtin);
 }
