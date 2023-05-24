f1ef435a371156dc7ac914e7d381bce9c2f477e2
JoJoGDS <george.latiko@gmail.com>
2023-05-24 14:56:53 -0700

    1. simple shell 0.1

--- builtin.c
+++ builtin.c
@@ -1,61 +1,100 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
+#include "shell.h"
 
-#define MAXLINE 1024 /* maximum length of a command line */
-#define PROMPT "$ " /* prompt symbol */
-
-/**
- * main - a simple UNIX command line interpreter
- * Return: 0 on success, 1 on failure
- */
-int main(void)
+/* Exit the shell with a given status */
+int _myexit(info_t *info)
 {
-    char line[MAXLINE]; /* buffer for the command line */
-    char *argv[MAXLINE]; /* array of arguments */
-    int argc; /* number of arguments */
-    pid_t pid; /* process id */
-    int status; /* exit status of the child process */
+  int status;
 
-    while (1) /* main loop */
+  /* Check if there is an exit argument */
+  if (info->argv[1])
+  {
+    /* Convert the argument to an integer */
+    status = _erratoi(info->argv[1]);
+    /* If the argument is invalid, print an error and return */
+    if (status == -1)
     {
-        printf(PROMPT); /* print the prompt */
-        if (fgets(line, MAXLINE, stdin) == NULL) /* read a line from stdin */
-        {
-            printf("\n"); /* print a new line */
-            break; /* exit the loop if end of file is reached */
-        }
-        if (line[strlen(line) - 1] == '\n') /* remove the trailing newline */
-            line[strlen(line) - 1] = '\0';
-        if (line[0] == '\0') /* ignore empty lines */
-            continue;
-        argc = 0; /* initialize the argument count */
-        argv[argc++] = strtok(line, " "); /* split the line by spaces and store the first argument */
-        while ((argv[argc++] = strtok(NULL, " ")) != NULL); /* store the rest of the arguments */
-        argc--; /* decrement the argument count to exclude the NULL pointer */
-        pid = fork(); /* create a new process */
-        if (pid < 0) /* fork failed */
-        {
-            perror("fork"); /* print an error message */
-            exit(1); /* exit with failure */
-        }
-        else if (pid == 0) /* child process */
-        {
-            execvp(argv[0], argv); /* execute the command with arguments */
-            perror(argv[0]); /* print an error message if execvp failed */
-            exit(1); /* exit with failure */
-        }
-        else /* parent process */
-        {
-            wait(&status); /* wait for the child to terminate */
-            if (WIFEXITED(status)) /* child exited normally */
-                printf("Child exited with status %d\n", WEXITSTATUS(status)); /* print the exit status */
-            else if (WIFSIGNALED(status)) /* child was terminated by a signal */
-                printf("Child terminated by signal %d\n", WTERMSIG(status)); /* print the signal number
-*/
-        }
+      info->status = 2;
+      print_error(info, "Illegal number: ");
+      _eputs(info->argv[1]);
+      _eputchar('\n');
+      return (1);
     }
-    return (0); /* exit with success */
+    /* Set the error number to the argument value */
+    info->err_num = status;
+    /* Return -2 to indicate exit */
+    return (-2);
+  }
+  /* If there is no argument, set the error number to -1 */
+  info->err_num = -1;
+  /* Return -2 to indicate exit */
+  return (-2);
+}
+
+/* Change the current directory of the process */
+int _mycd(info_t *info)
+{
+  char *s, *dir, buffer[1024];
+  int ret;
+
+  /* Get the current working directory */
+  s = getcwd(buffer, 1024);
+  if (!s)
+    _puts("TODO: >>getcwd failure emsg here<<\n");
+
+  /* If there is no argument, change to the home directory */
+  if (!info->argv[1])
+  {
+    dir = _getenv(info, "HOME=");
+    if (!dir)
+      ret = chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+    else
+      ret = chdir(dir);
+  }
+  /* If the argument is "-", change to the previous directory */
+  else if (_strcmp(info->argv[1], "-") == 0)
+  {
+    if (!_getenv(info, "OLDPWD="))
+    {
+      _puts(s);
+      _putchar('\n');
+      return (1);
+    }
+    _puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+    ret = chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+  }
+  /* Otherwise, change to the given directory */
+  else
+    ret = chdir(info->argv[1]);
+
+  /* If the change failed, print an error */
+  if (ret == -1)
+  {
+    print_error(info, "can't cd to ");
+    _eputs(info->argv[1]), _eputchar('\n');
+  }
+  /* Otherwise, update the environment variables */
+  else
+  {
+    _setenv(info, "OLDPWD", _getenv(info, "PWD="));
+    _setenv(info, "PWD", getcwd(buffer, 1024));
+  }
+
+  return (0);
+}
+
+/* Display help information for built-in commands */
+int _myhelp(info_t *info)
+{
+  char **args;
+
+  args = info->argv;
+  
+  /* TODO: implement help functionality */
+
+  _puts("help call works. Function not yet implemented \n");
+
+  if (0)
+    _puts(*args); /* temp att_unused workaround */
+
+  return (0);
 }
